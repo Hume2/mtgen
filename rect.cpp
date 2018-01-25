54ab7046540fc5eb39f9dd50149e920a0f011ce4
Hume2 <teratux.mail@gmail.com>
2018-01-25 12:21:50 +0100

    Add Rect class

---
+++ rect.cpp
@@ -0,0 +1,19 @@
+#include "rect.h"
+
+Rect::Rect() :
+  x1(0),
+  y1(0),
+  x2(0),
+  y2(0)
+{
+
+}
+
+Rect::Rect(int x1_, int y1_, int x2_, int y2_):
+  x1(x1_),
+  y1(y1_),
+  x2(x2_),
+  y2(y2_)
+{
+
+}
---
+++ rect.h
@@ -0,0 +1,14 @@
+#ifndef RECT_H
+#define RECT_H
+
+
+class Rect
+{
+  public:
+    Rect();
+    Rect(int x1_, int y1_, int x2_, int y2_);
+
+    int x1, y1, x2, y2;
+};
+
+#endif // RECT_H
