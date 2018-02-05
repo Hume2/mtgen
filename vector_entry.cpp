cb4b86063d657a6845fc1e8dfaa0dbdff3aa1afe
Hume2 <teratux.mail@gmail.com>
2018-02-05 09:30:01 +0100

    VectorEntry: Generating inside ortoplex

--- vector_entry.cpp
+++ vector_entry.cpp
@@ -1,3 +1,4 @@
+#include <math.h>
 #include <random>
 
 #include "vector_entry.h"
@@ -10,12 +11,26 @@ VectorEntry::VectorEntry(boost::numeric::ublas::vector<double> vector_, bool is_
 }
 
 VectorEntry::VectorEntry(const boost::numeric::ublas::vector<double>& min,
-                         const boost::numeric::ublas::vector<double>& max, bool is_true_) :
+                         const boost::numeric::ublas::vector<double>& max,
+                         Shape shape, bool is_true_) :
   vector(min.size()),
   is_true(is_true_)
 {
-  for (int i = vector.size()-1; i >= 0; --i) {
-    vector[i] = (double)rand() / RAND_MAX * (max[i] - min[i]) + min[i];
+  using namespace boost::numeric::ublas;
+  switch (shape) {
+    case CUBE: {
+      for (int i = vector.size()-1; i >= 0; --i) {
+        vector[i] = (double)rand() / RAND_MAX * (max[i] - min[i]) + min[i];
+      }
+    } break;
+    case ORTOPLEX: {
+      double sum = 1;
+      for (int i = vector.size()-1; i >= 0; --i) {
+        vector[i] = (1 - pow((double)rand() / RAND_MAX, 1/(i+1)))*sum;
+        sum -= vector[i];
+        vector[i] = (max[i] - min[i])*vector[i] + (max[i] + min[i])/2;
+      }
+    } break;
   }
 }
 
--- vector_entry.h
+++ vector_entry.h
@@ -6,9 +6,12 @@
 class VectorEntry
 {
   public:
+    enum Shape{CUBE, ORTOPLEX};
+
     VectorEntry(boost::numeric::ublas::vector<double> vector_, bool is_true_ = true);
     VectorEntry(const boost::numeric::ublas::vector<double>& min,
-                const boost::numeric::ublas::vector<double>& max, bool is_true_ = false);
+                const boost::numeric::ublas::vector<double>& max,
+                Shape shape = CUBE, bool is_true_ = false);
 
     boost::numeric::ublas::vector<double> vector;
     bool is_true;
